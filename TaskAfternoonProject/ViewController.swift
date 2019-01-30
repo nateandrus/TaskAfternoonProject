a154bbb0b612d9b119f5cf2e5b753ac87d640b03
Nathan Andrus <nathanpandrus@gmail.com>
2019-01-30 10:11:58 -0700

    Initial Commit

---
+++ TaskAfternoonProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskAfternoonProject
+//
+//  Created by Nathan Andrus on 1/30/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
